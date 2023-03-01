bc67e37179e3426a0fa8532ccc0eabc14712ae6c
SMSLink.ro <contact@smslink.ro>
2023-03-01 15:16:37 +0200

    PowerShell 3.0 Example

--- powershell-3-example.ps1
+++ powershell-3-example.ps1
@@ -9,7 +9,7 @@
 #
 
 #
-#  Requires Microsoft PowerShell 3.0
+#  Requires Microsoft PowerShell 3.0 
 #
 #  Invoke-WebRequest cmdlet was introduced in PowerShell 3.0
 #    Learn More at https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/invoke-webrequest
