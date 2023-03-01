37fb1a22c3cd4b36c5f32837612548ea8c2cacc8
SMSLink.ro <contact@smslink.ro>
2023-03-01 14:55:27 +0200

    Create powershell-3-via-command-prompt.bat

---
+++ powershell-3-via-command-prompt.bat
@@ -0,0 +1,16 @@
+REM
+REM  HTTPS API Endpoint:  https://secure.smslink.ro/sms/gateway/communicate/index.php
+REM  HTTP API Endpoint:   http://www.smslink.ro/sms/gateway/communicate/index.php
+REM
+
+REM
+REM  Get your SMSLink / SMS Gateway Connection ID and Password from 
+REM    https://www.smslink.ro/get-api-key/
+REM
+
+REM
+REM  Invoke-WebRequest cmdlet was introduced in PowerShell 3.0
+REM    Learn More at https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/invoke-webrequest
+REM
+
+powershell.exe Invoke-WebRequest -Uri 'https://secure.smslink.ro/sms/gateway/communicate/index.php?connection_id=MyConnectionID&password=MyConnectionPassword&to=07xyzzzzzz&message=TestMessage'
